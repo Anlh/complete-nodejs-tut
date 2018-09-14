b4396883dc80196f104fbcefb1e161f12f4aab58
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-09-14 13:40:30 +0100

    -> Done lesson "Getting input from the user" through the command line using process.argv

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+let command = process.argv[2];
+
+console.log('Command: ', command);
+console.log('Arguments: ', process.argv);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'delete') {
+    console.log('Deleting note');
+} else {
+    console.log('Command not recognized');
+}
+
