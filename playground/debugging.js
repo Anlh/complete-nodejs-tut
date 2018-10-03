153e30b424d4b1d7e5e55087d8bde753518954c8
Anlh <heldern19@gmail.com>
2018-10-03 15:30:45 +0100

    -> Done Lesson 21 "Debugging via Chrome Dev Tools"

--- playground/debugging.js
+++ playground/debugging.js
@@ -12,9 +12,7 @@ debugger;
 
 console.log(person);
 
-// use inspect flag when we want to debug the app on the terminal
-// List of terminal debug commands
-// [c] -> continue (will go to the next breakpoint) breakpoints are declared in code with the keyword debugger
-// [n] -> next (Go to the next line)
-// [list(n)] n -> line number (go to a specific line number)
-// [repl] -> Used to evaluate/manipulate the variables like we use evaluate in the browser terminal of chrome
+// Or you can use Chrome dev tools to inspect the code by writing the follow line on the terminal
+// $ node/nodemon --inspect-brk [file_path] and execute it
+
+// Open chrome://inspect in the url bar inside chrome browser to open the file
