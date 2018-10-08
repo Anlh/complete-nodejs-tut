cda8106e6c872269427315a1654e8db80fd000bd
Anlh <heldern19@gmail.com>
2018-10-08 19:04:00 +0100

    -> Done Lesson 30 "What's MAKES up an HTTP Request"

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,16 @@
 const request = require('request');
-
+const {KEY_NAME} = require('./KEY');
 
 request({
-    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=ark7A5XuklLU9nAr0WPvUsGb5RAkMmIj&location=rua%20carreira%20229%20rio%20tinto',
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${KEY_NAME}&location=rua%20carreira%20229%20rio%20tinto`,
     json: true
 }, (error, response, body) => {
+    const lat = body.results[0].locations[0].latLng.lat;
+    const lng = body.results[0].locations[0].latLng.lng;
     // Pretty printing objects
     console.log(JSON.stringify(body, undefined, 2));
+
+    console.log(`Latitude ${lat}`);
+    console.log(`----------------`);
+    console.log(`Longitude: ${lng}`);
 });
