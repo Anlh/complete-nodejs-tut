b10694c195e5578d1037d56368c41e2446d61d1d
Anlh <heldern19@gmail.com>
2018-10-10 22:43:42 +0100

    -> Done Lesson 31 "Encode/decode user input" using encodeURIComponent function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,26 @@
 const request = require('request');
 const {KEY_NAME} = require('./KEY');
+const yargs = require('yargs');
+
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const ENCODED_ADDRESS = encodeURIComponent(argv.a);
+
 
 request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${KEY_NAME}&location=rua%20carreira%20229%20rio%20tinto`,
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${KEY_NAME}&location=${ENCODED_ADDRESS}`,
     json: true
 }, (error, response, body) => {
     const lat = body.results[0].locations[0].latLng.lat;
