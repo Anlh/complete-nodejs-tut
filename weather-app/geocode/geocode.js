e8d9fbce7ac0ec136f7b3f8c273c29d4e001a99f
Anlh <heldern19@gmail.com>
2018-10-16 23:01:51 +0100

    Done lesson 34 "Wiring  up weather search"

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -1,13 +1,12 @@
 const request = require('request');
-const {
-    KEY_NAME
-} = require('../KEY');
+const fs = require('fs');
+const KEYS = require('../API_KEYS.json');
 
 const geocodeAddress = (address, callback) => {
     const ENCODED_ADDRESS = encodeURIComponent(address);
 
     request({
-        url: `http://www.mapquestapi.com/geocoding/v1/address?key=${KEY_NAME}&location=${ENCODED_ADDRESS}`,
+        url: `http://www.mapquestapi.com/geocoding/v1/address?key=${KEYS["geolocation-api-key"]}&location=${ENCODED_ADDRESS}`,
         json: true
     }, (error, response, body) => {
         if (error) {
